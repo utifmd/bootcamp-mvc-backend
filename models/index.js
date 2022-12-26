e9670da077c59a2949c15380ec6a95580fdd6378
Utif Milkedori <utifmd@gmail.com>
2022-12-26 15:06:57 +0700

    vercel deployment #2

--- models/index.js
+++ models/index.js
@@ -4,17 +4,23 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+const pg = require("pg")
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+
+config.dialectModule = pg
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+console.log(config)
 
 fs
   .readdirSync(__dirname)
